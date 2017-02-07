5321e19a09d08f35cddf4fa13453f920434ad99c
R3Ds <fpoolboy@gmail.com>
2017-02-06 22:20:31 -0500

    [N4F26O] February android-7.1.1_r22

--- angler/proprietary/framework/Android.mk
+++ angler/proprietary/framework/Android.mk
@@ -1,4 +1,5 @@
-# Copyright (C) 2016 The Pure Nexus Project
+# Copyright (C) 2015 The Pure Nexus Project
+# Copyright (C) 2016 Benzo Rom
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,6 +16,15 @@
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),angler)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := com.google.widevine.software.drm
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := com.google.widevine.software.drm.jar
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_MODULE_CLASS := JAVA_LIBRARIES
+LOCAL_MODULE_SUFFIX := $(COMMON_JAVA_PACKAGE_SUFFIX)
+include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := qcrilhook
